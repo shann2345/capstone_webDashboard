0c972c6282570635ebb943fec311d00303cae601
Shann <lagdaanshannarlly@gmail.com>
2025-06-12 15:45:26 +0800

    laravel setup with login

---
+++ resources/views/register/signup.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Instructor Sign Up</title>
+    </head>
+<body>
+    <h1>Register as an Instructor</h1>
+
+    @if ($errors->any())
+        <div>
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+
+   
+    <form method="POST" action="{{ route('instructor.register.post') }}">
+        @csrf
+
+        <label for="name">Name:</label><br>
+        <input type="text" id="name" name="name" value="{{ old('name') }}" required autofocus><br><br>
+
+        <label for="email">Email:</label><br>
+        <input type="email" id="email" name="email" value="{{ old('email') }}" required><br><br>
+
+        <label for="password">Password:</label><br>
+        <input type="password" id="password" name="password" required><br><br>
+
+        <label for="password_confirmation">Confirm Password:</label><br>
+        <input type="password" id="password_confirmation" name="password_confirmation" required><br><br>
+
+        <button type="submit">Register as Instructor</button>
+    </form>
+
+    <p>Already have an account? <a href="{{ route('login') }}">Login here</a></p>
+</body>
+</html>
